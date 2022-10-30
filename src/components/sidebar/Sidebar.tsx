f9881514d8629e66c0f7bb593b215ddc3166a6e2
Aniket Pathak <aniketpathak028@gmail.com>
2022-10-30 11:01:25 +0530

    create sidebar structure

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,27 +1,51 @@
-import React, { FC } from 'react';
-import './Sidebar.styles.css';
+import React from 'react';
+import { FC } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Sidebar: FC = () => {
-  return (
-    <>
-      <div className="Sidebar">
-        <div className="ProjectName">
-          <input type="text" placeholder="Project Name" />
-        </div>
+  const sidebarRef = useRef(null);
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState(268);
 
-        <div className="Steps">Steps:</div>
+  const startResizing = React.useCallback((mouseDownEvent: any) => {
+    setIsResizing(true);
+  }, []);
 
-        <ul className="SidebarList">
-          <li className="row">
-            <div className="title">Blur</div>
-            <div className="title">Edge Detection</div>
-            <div className="title">Region of Interest (ROI)</div>
-            <div className="title">Transform</div>
-          </li>
-        </ul>
-      </div>
-    </>
+  const stopResizing = React.useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = React.useCallback(
+    (mouseMoveEvent: any) => {
+      if (isResizing) {
+        setSidebarWidth(mouseMoveEvent.clientX - sidebarRef.current.getBoundingClientRect().left);
+      }
+    },
+    [isResizing]
   );
-};
+
+  React.useEffect(() => {
+    window.addEventListener('mousemove', resize);
+    window.addEventListener('mouseup', stopResizing);
+    return () => {
+      window.removeEventListener('mousemove', resize);
+      window.removeEventListener('mouseup', stopResizing);
+    };
+  }, [resize, stopResizing]);
+
+  return (
+    <div className="app-container">
+      <div
+        ref={sidebarRef}
+        className="app-sidebar"
+        style={{ width: sidebarWidth }}
+        onMouseDown={(e) => e.preventDefault()}>
+        <div className="app-sidebar-content" />
+        <div className="app-sidebar-resizer" onMouseDown={startResizing} />
+      </div>
+      <div className="app-frame" />
+    </div>
+  );
+}
 
 export default Sidebar;
