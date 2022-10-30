741940a5cf4323739fadeb79914ee65778bc688c
Aniket Pathak <aniketpathak028@gmail.com>
2022-10-30 11:18:58 +0530

    add comments for functions and hooks

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import { FC } from 'react';
-import { useState, useEffect, useRef } from 'react';
-import './Sidebar.styles.css';
+import React, { FC } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react'; // importing Hooks
+
+import './Sidebar.styles.css'; // importing sidebar styles
 
 const Sidebar: FC = () => {
+  // states
   const sidebarRef = useRef(null);
   const [isResizing, setIsResizing] = useState(false);
   const [sidebarWidth, setSidebarWidth] = useState(268);
 
-  const startResizing = React.useCallback((mouseDownEvent: any) => {
+  // sets isResizing to true
+  const startResizing = useCallback((mouseDownEvent: any) => {
     setIsResizing(true);
   }, []);
 
-  const stopResizing = React.useCallback(() => {
+  // sets isResizing to false
+  const stopResizing = useCallback(() => {
     setIsResizing(false);
   }, []);
 
-  const resize = React.useCallback(
+  // changes the sidebar width while isResizing is true
+  const resize = useCallback(
     (mouseMoveEvent: any) => {
       if (isResizing) {
+        // set the new sidebar width to = current X coordinate of mouse (X) - left X coordinate of sidebar
         setSidebarWidth(mouseMoveEvent.clientX - sidebarRef.current.getBoundingClientRect().left);
       }
     },
     [isResizing]
   );
 
-  React.useEffect(() => {
+  // addEventListeners as soon as the component mounts
+  useEffect(() => {
     window.addEventListener('mousemove', resize);
     window.addEventListener('mouseup', stopResizing);
     return () => {
