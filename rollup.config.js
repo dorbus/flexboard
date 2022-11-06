f462b4e35b14fcdc6a36394bfd1f2ff7650a4fa0
Divyanshu Shekhar <imdshekhar@gmail.com>
2022-11-06 13:29:35 +0530

    rollup types issue fixed

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import packageJson from './package.json' assert { type: 'json' };
 
@@ -41,7 +41,7 @@ export default [
     ]
   },
   {
-    input: 'dist/esm/index.d.ts',
+    input: 'dist/esm/src/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
     external: [/\.css$/]
